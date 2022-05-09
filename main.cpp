0a98f81658b5ec928cd097e2058d716f9048225b
Aiden Crilley <aidencri@udel.edu>
2022-05-09 13:21:19 -0400

    added method for job creation

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int cTime, mainMem, numSerial, timeQ;
 
-struct job {
+struct Job {
     int time;
     int jobNum; 
     int mainMem; 
@@ -16,8 +16,20 @@ struct job {
     int priority;
 };
 
-queue<job> HoldQ1;
-queue<job> HoldQ2;
+queue<Job> HoldQ1;
+queue<Job> HoldQ2;
+
+Job jobCreate(int time, int jN, int mM, int nS, int rT, int p) {
+    Job j;
+    j.time = time;
+    j.jobNum = jN;
+    j.mainMem = mM;
+    j.numSerial = nS;
+    j.runTime = rT;
+    j.priority = p;
+
+    return j;
+}
 
 void systemConfiguration(int t, int mM, int nS, int tQ) {
     cTime = t;
@@ -28,12 +40,25 @@ void systemConfiguration(int t, int mM, int nS, int tQ) {
     cout << "Time " << cTime << ' ' << "Mem " << mainMem << ' ' << "Serial " << numSerial << ' ' << "Quantum " << timeQ;
 }
 
-void jobArrival(int time, int jobNum, int mainMem, int numSerial, int runTime, int priority) {
+void jobArrival(int time, int jN, int mM, int nS, int rT, int p) {
+    Job j = jobCreate(time, jN, mM, nS, rT, p);
+
     // Case 1: If there is not enough total main memory or total number of devices in the system for the job, 
     // the job is rejected never gets to one of the Hold Queues. 
+    if (mM > mainMem || nS > numSerial) {
+        cout << "Job rejected due to lack of resources/\n";
+    }
 
     // Case 2: If  there  is  not  enough  available  main  memory  for  the  job,  the  job  is  put  in  one  of  the  Hold 
     // Queues, based on its priority, to wait for enough available main memory.
+    else if (mM > mainMem) {
+        if (p == 1) {
+
+        }
+        else if (p == 2) {
+
+        }
+    }
 
     // Case 3: If there is enough main memory for the job, then a process is created for the job, the required 
     // main memory is allocated to the process, and the process is put in the Ready Queue.  
